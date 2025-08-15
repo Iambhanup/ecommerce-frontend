4d46b6a824a4de600f1dc7797dd0cf83fdb55769
Iambhanup <163726740+Iambhanup@users.noreply.github.com>
2025-08-15 18:13:52 +0530

    okk

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -5,7 +5,7 @@ import { Link, Outlet } from "react-router-dom";
 const AboutUs = () => {
   return (
     <div className="about-container">
-      <h2>Welcome to the DevOps class</h2>
+      <h2>Welcome to the Ecommerce Website</h2>
       <p>
         Welcome to our E-Commerce platform, your one-stop shop for the latest and greatest in electronics!
         We specialize in high-quality products, including computers, mobiles, laptops, and accessories.
